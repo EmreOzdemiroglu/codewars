fc70c9a2c32927d424b255aed26750d49266c795
EmreOzdemiroglu <31511271+EmreOzdemiroglu@users.noreply.github.com>
2020-07-22 11:38:32 +0300

    Add files via upload

---
+++ grasshopper-summation.py
@@ -0,0 +1,26 @@
+def summation(num):
+    pass
+    d=[]
+    for i in range(num):
+        i+=1
+        d.append(i)
+    return sum(d)
+
+
+
+def summation(num):
+    return sum(xrange(num+1)
+
+
+def summation(num):
+    return (1+num)*num/2
+
+
+summation = lambda n:n*-~n>>1
+
+summation = lambda x: sum(range(1,x+1))
+
+def summation(num):
+    return sum([x for x in rxrange(num+1)])
+
+
